f552d0cd98817dfabb16ceb897ca5cdf1e36bae3
Ridhwan Oladejo <ridhwan.oladejo@9mobile.com.ng>
2021-05-18 10:40:56 +0100

    Code debug

--- src/main/java/com/esd/sercom/bulksms/service/usermanagement/UserServiceImpl.java
+++ src/main/java/com/esd/sercom/bulksms/service/usermanagement/UserServiceImpl.java
@@ -40,7 +40,7 @@ public class UserServiceImpl implements UserService{
         userToBeRegistered.setCorrelationId(UUID.randomUUID().toString());
         UserEntity userEntity = userEntityRepo.save(userToBeRegistered);
         String jwt = Utilities.createJWT(userEntity.getEmail(),36000000,null);
-        String body = this.createUserMail(user, "");
+        String body = this.createUserMail(userDetails, "");
         EmailModel emailModel = new EmailModel();
         emailModel.setFrom("bulksms-noreply@9mobile.com.ng");
         emailModel.setSubject("Please confirm your new 9mobile Bulksms account");
