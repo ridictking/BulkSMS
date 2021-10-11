85c12e1cbf92ca7c6838bc71332e3de9769dad98
Ridhwan Oladejo <ridhwan.oladejo@9mobile.com.ng>
2021-10-11 21:01:46 +0100

    Disbled cors

--- src/main/java/com/esd/sercom/bulksms/config/CorsConfig.java
+++ src/main/java/com/esd/sercom/bulksms/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedMethods("*");
             }
         };
     }
